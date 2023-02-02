261ca901e71d53d0769acdcf5af312fa7290acb7
Jackson <z5397791@ad.unsw.edu.au>
2023-02-03 09:16:45 +1100

    Wrap app component around supabase provider

--- src/index.js
+++ src/index.js
@@ -4,16 +4,19 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { createClient } from "@supabase/supabase-js";
+import { SessionContextProvider } from "@supabase/auth-helpers-react";
 
 const supabase = createClient(
   "https://udcmatwaayidfdqnsric.supabase.co",
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVkY21hdHdhYXlpZGZkcW5zcmljIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzUzNzQwMTEsImV4cCI6MTk5MDk1MDAxMX0.zyUdV5XBDUmxYjvoQo2eS98Gh9-clMqwakhjO-eVmNU"
-)
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <SessionContextProvider>
+      <App />
+    </SessionContextProvider>
   </React.StrictMode>
 );
 
