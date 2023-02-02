7f0c53bde676a6c0f55e3007f00704ded1a24a3a
Jackson <z5397791@ad.unsw.edu.au>
2023-02-03 09:23:48 +1100

    Add supabase client into session provider in index

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ const supabase = createClient(
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <SessionContextProvider>
+    <SessionContextProvider supabaseClient={supabase}>
       <App />
     </SessionContextProvider>
   </React.StrictMode>
