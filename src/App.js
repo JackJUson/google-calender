1efa37c0c2ed891770b633ad2e908ef130b486d4
Jackson <z5397791@ad.unsw.edu.au>
2023-02-03 09:30:30 +1100

    Written implementation for google sign in function using supabase oauth

--- src/App.js
+++ src/App.js
@@ -5,6 +5,19 @@ function App() {
   const session = useSession(); // tokens
   const supabase = useSupabaseClient(); // talk to supabase!
 
+  async function googleSignIn() {
+    const { error } = await supabase.auth.signInWithOAuth({
+      provider: "google",
+      options: {
+        scopes: "https://www.googleapis.com/auth/calendar",
+      },
+    });
+    if (error) {
+      alert("Error logging in to Google Provider with Supabase");
+      console.log(error);
+    }
+  }
+
   return (
     <div className="App">
       <div style={{ width: "400px", margin: "30px auto" }}>
