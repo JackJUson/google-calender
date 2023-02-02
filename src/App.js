0d6c590d14d0842a26bd97a288133c48c4a0374a
Jackson <z5397791@ad.unsw.edu.au>
2023-02-03 09:24:16 +1100

    Add ternary condition to show login option or header in home page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import './App.css';
-import { useSession, useSupabaseClient } from '@supabase/auth-helpers-react';
+import "./App.css";
+import { useSession, useSupabaseClient } from "@supabase/auth-helpers-react";
 
 function App() {
   const session = useSession(); // tokens
-  const supabse = useSupabaseClient(); // talk to supabase!
+  const supabase = useSupabaseClient(); // talk to supabase!
 
   return (
     <div className="App">
+      <div style={{ width: "400px", margin: "30px auto" }}>
+        {session ? (
+          <>
+            <h2>Hey there {session.user.email}</h2>
+          </>
+        ) : (
+          <>
+            <button onClick={() => googleSignIn()}>Sign In With Google</button>
+          </>
+        )}
+      </div>
     </div>
   );
 }
